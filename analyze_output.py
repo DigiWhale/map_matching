3a2b361449bcf3809c510593016034a17a0f659a
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-08 17:55:16 -0400

    1

--- analyze_output.py
+++ analyze_output.py
@@ -9,8 +9,10 @@ df['geometry'] = df['mgeom']
 # df['geometry'] = df['mgeom'].map(wkt.loads)
 line_string = []
 for index, row in df.iterrows():
-    line_string.append(df['geometry'].iloc[index])
-    print(df['geometry'].iloc[index])
+  line = df['geometry'].iloc[index]
+  line_string.append(line)
+  if len(line) > 2:
+    print(line)
 print(df.geometry.head())
 crs = {'init': 'epsg:4326'}
 gdf = gpd.GeoDataFrame(df, crs=crs).set_geometry('geometry')
