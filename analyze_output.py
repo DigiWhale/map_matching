a4e599baab9417103bf17e2be1787102667441d3
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-08 18:10:23 -0400

    1

--- analyze_output.py
+++ analyze_output.py
@@ -5,16 +5,17 @@ from shapely import wkt
 
 df = pd.read_csv('output.csv', sep=';')
 df = df[0: -10]
-df['geometry'] = df['mgeom']
-# df['geometry'] = df['mgeom'].map(wkt.loads)
+# df['geometry'] = df['mgeom']
+df['geometry'] = df['mgeom'].apply(wkt.loads)
 line_string = []
 for index, row in df.iterrows():
   line = df['geometry'].iloc[index]
-  if len(line) == 69:
-    print(index)
-    line_string.append(df['geometry'][index].apply(wkt.loads))
-    print(type(line))
-    print(len(line))
+  print(line)
+  # if len(line) == 69:
+  #   print(index)
+  #   line_string.append(df['geometry'][index].apply(wkt.loads))
+  #   print(type(line))
+  #   print(len(line))
 # print(df.geometry.head())
 crs = {'init': 'epsg:4326'}
 gdf = gpd.GeoDataFrame(df, crs=crs).set_geometry('geometry')
