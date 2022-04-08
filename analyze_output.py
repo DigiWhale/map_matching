f913c441419355f282131ac664cf51c19c77ea66
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-08 11:56:38 -0400

    1

--- analyze_output.py
+++ analyze_output.py
@@ -5,7 +5,7 @@ from shapely import wkt
 
 df = pd.read_csv('output.csv', sep=';')
 print(df.head())
-df['pgeom'] = df['pgeom'].apply(wkt.loads)
+df['geometry'] = df['pgeom']
 crs = {'init': 'epsg:4326'}
 gdf = gpd.GeoDataFrame(df, crs=crs).set_geometry('pgeom')
 print(gdf.head())
