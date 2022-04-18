34c28633d882338ba7b81ded33f7e8b908182e1a
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-18 19:02:21 -0400

    1

--- match.sh
+++ match.sh
@@ -1,6 +1,10 @@
 stmatch\
  --network  maryland/edges.shp\
  --gps ../road_network/master_log.csv\
+ --gps_x jetson_rpi_lng\
+ --gps_y jetson_rpi_lat\
+ --gps_timestamp timestamp\
+ --gps_id Id\
  -k 16 -r 0.005 -e 0.0005\
  --vmax 0.0002\
  --output maryland_match.txt
